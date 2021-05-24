5f52ecf13dfba697f220979fecb6612230623069
Cole Kelly <Cole.jkelly1@yahoo.com>
2021-05-24 00:46:06 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const api = {
+    key: "M0BV1EKQATYM"
+    baseurl: "http://api.timezonedb.com"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListerner('keypress',setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.vaule);
+
+    }
+}
+
+function getResults (query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = datebuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow= document.querySelector('.hi-low');
+    hilow.innerText = `${weather.main.temp_min} % / ${weather.main.temp_maxºc}`;
+}
+
+
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sun","Mon","Tues","Wed","Thurs","Fri","Sat"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+
+}
